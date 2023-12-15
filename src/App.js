3d5f2eab155aefbcc8de873f47b107c95aef84df
Bhoomika <bhoomikahm18@gmail.com>
2023-12-15 13:13:22 +0530

    Changing the home and side navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Sidenav from './Sidenav';
+import Home from './pages/Home';
+import About from './pages/About';
+import Settings from './pages/Settings';
 
 function App() {
   return (
     <>
-      <Sidenav />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/about' exact element={<About />} />
+          <Route path='/settings' exact element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
