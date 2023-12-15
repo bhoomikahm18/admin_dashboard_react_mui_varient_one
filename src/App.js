d9a7c15798821d907b4cf5b11b87b3f2bca9f825
Bhoomika <bhoomikahm18@gmail.com>
2023-12-15 12:43:21 +0530

    Installing required packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sidenav from './Sidenav';
 
 function App() {
   return (
     <>
-      Hello World
+      <Sidenav />
     </>
   );
 }
