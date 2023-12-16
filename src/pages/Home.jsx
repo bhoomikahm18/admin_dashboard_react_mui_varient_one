ee1bb61fa4f8d435c9c0ba4619fffda64a08df76
Bhoomika <bhoomikahm18@gmail.com>
2023-12-16 16:24:48 +0530

    Adding accordion to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,57 @@
 import React from 'react'
-import Sidenav from '../components/Sidenav'
+import Sidenav from '../components/Sidenav.jsx'
 import { Box, Card, CardContent, Stack, Typography } from '@mui/material'
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar.jsx';
 import Grid from '@mui/material/Grid';
 import '../Dash.css';
 import StorefrontIcon from '@mui/icons-material/Storefront';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import AccordionDash from '../components/AccordionDash.jsx';
 
 export default function Home() {
     return (
         <>
-            <Navbar />
-            <Box height={70} />
-            <Box sx={{ display: "flex" }}>
-                <Sidenav />
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={8}>
-                            <Stack spacing={2} direction={"row"}>
-                                <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="div">
-                                            $500.00
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                                <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="div">
-                                            $900.00
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Stack>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Stack spacing={2} >
-                                <Card sx={{ maxWidth: 345 }}>
+            <div className='bgcolor'>
+                <Navbar />
+                <Box height={70} />
+                <Box sx={{ display: "flex" }}>
+                    <Sidenav />
+                    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={8}>
+                                <Stack spacing={2} direction={"row"}>
+                                    <Card sx={{ minWidth: 49 + "%", height: 150 }} className='gradient'>
+                                        <CardContent>
+                                            <div className='iconstyle'>
+                                                <CreditCardIcon />
+                                            </div>
+                                            <Typography gutterBottom variant="h5" component="div" sx={{ color: "#ffffff" }}>
+                                                $500.00
+                                            </Typography>
+                                            <Typography gutterBottom variant="body2" component="div" sx={{ color: "#ccd1d1" }}>
+                                                Total Earnings
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                    <Card sx={{ minWidth: 49 + "%", height: 150 }} className='gradientlight'>
+                                        <CardContent>
+                                            <div className='iconstyle'>
+                                                <ShoppingBagIcon />
+                                            </div>
+                                            <Typography gutterBottom variant="h5" component="div" sx={{ color: "#ffffff" }}>
+                                                $900.00
+                                            </Typography>
+                                            <Typography gutterBottom variant="body2" component="div" sx={{ color: "#ccd1d1" }}>
+                                                Total Orders
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Stack spacing={2} >
+                                    <Card className='gradientlight'>
                                         <Stack spacing={2} direction={"row"} >
                                             <div className='iconstyle'>
                                                 <StorefrontIcon />
@@ -46,10 +62,10 @@ export default function Home() {
                                                 <span className='pricesubtitle'> Total Income</span>
                                             </div>
                                         </Stack>
-                                </Card>
-                                <Card sx={{ maxWidth: 345 }}>
+                                    </Card>
+                                    <Card >
                                         <Stack spacing={2} direction={"row"} >
-                                            <div className='iconstyle'>
+                                            <div className='iconstyleblack'>
                                                 <StorefrontIcon />
                                             </div>
                                             <div className='paddingall'>
@@ -58,27 +74,33 @@ export default function Home() {
                                                 <span className='pricesubtitle'> Total Income</span>
                                             </div>
                                         </Stack>
+                                    </Card>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                        <Box height={20} />
+                        <Grid container spacing={2}>
+                            <Grid item xs={8}>
+                                <Card sx={{ height: 60 + "vh" }}>
+                                    <CardContent>
+
+                                    </CardContent>
                                 </Card>
-                            </Stack>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Card sx={{ height: 60 + "vh" }}>
+                                    <CardContent>
+                                        <div className='paddingall'>
+                                            <span className='pricetitle'>Popular Products</span>
+                                        </div>
+                                        <AccordionDash />
+                                    </CardContent>
+                                </Card>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                    <Box height={20} />
-                    <Grid container spacing={2}>
-                        <Grid item xs={8}>
-                            <Card sx={{ height: 60 + "vh" }}>
-                                <CardContent>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Card sx={{ height: 60 + "vh" }}>
-                                <CardContent>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
+                    </Box>
                 </Box>
-            </Box>
+            </div>
         </>
     )
 }
