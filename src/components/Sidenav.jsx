8b089319c52cc1d65205db032ac5dcb48a7b8f0f
Bhoomika <bhoomikahm18@gmail.com>
2023-12-16 13:41:49 +0530

    Adding nav bar for all pages

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -13,6 +13,8 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
+import { useNavigate } from 'react-router-dom';
+import { useAppStore } from '../appStore';
 
 const drawerWidth = 240;
 
@@ -65,20 +67,23 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 
 export default function Sidenav() {
     const theme = useTheme();
-    const [open, setOpen] = React.useState(true);
+    /*const [open, setOpen] = React.useState(true);*/
+    // const updateOpen = useAppStore((state) => state.updateOpen);
+    const open = useAppStore((state) => state.dopen);
+    const navigate = useNavigate();
 
     return (
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
             <Drawer variant="permanent" open={open}>
-                <DrawerHeader>
+                {/* <DrawerHeader>
                     <IconButton onClick={() => setOpen(!open)}>
                         {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
                     </IconButton>
-                </DrawerHeader>
+                </DrawerHeader> */}
                 <Divider />
                 <List>
-                    <ListItem disablePadding sx={{ display: 'block' }}>
+                    <ListItem disablePadding sx={{ display: 'block' }} onClick={() => { navigate("/") }}>
                         <ListItemButton
                             sx={{
                                 minHeight: 48,
@@ -98,7 +103,7 @@ export default function Sidenav() {
                             <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
                         </ListItemButton>
                     </ListItem>
-                    <ListItem disablePadding sx={{ display: 'block' }}>
+                    <ListItem disablePadding sx={{ display: 'block' }} onClick={() => { navigate("/about") }}>
                         <ListItemButton
                             sx={{
                                 minHeight: 48,
@@ -118,7 +123,7 @@ export default function Sidenav() {
                             <ListItemText primary="About" sx={{ opacity: open ? 1 : 0 }} />
                         </ListItemButton>
                     </ListItem>
-                    <ListItem disablePadding sx={{ display: 'block' }}>
+                    <ListItem disablePadding sx={{ display: 'block' }} onClick={() => { navigate("/settings") }}>
                         <ListItemButton
                             sx={{
                                 minHeight: 48,
@@ -140,9 +145,6 @@ export default function Sidenav() {
                     </ListItem>
                 </List>
             </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <DrawerHeader />
-            </Box>
         </Box>
     );
 }
